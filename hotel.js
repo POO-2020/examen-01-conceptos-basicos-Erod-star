2909443b3c2b5582e320d55a21d4bfcc35c0c8f6
Erod-star <erodriguez43@ucol.mx>
2020-03-03 18:05:34 -0600

    Añande avances

---
+++ hotel.js
@@ -0,0 +1,21 @@
+import Reservacion from "./reservacion.js";
+import Huesped from "./huesped.js";
+
+export default class Hotel {
+    constructor(nombre) {
+        this.nombre = nombre;
+        this.reservaciones = new Array();
+    }
+
+    getNumeHuespedes() {
+        this.reservaciones
+    }
+
+    getCostoReservaciones() {
+
+    }
+
+    print() {
+
+    }
+}
---
+++ main.js
@@ -0,0 +1,35 @@
+import Reservacion from "./reservacion.js"
+import Huesped from "./huesped.js"
+
+export default class Main {
+    constructor() { }
+
+    testHuesped() {
+        let x = new Huesped('Jose Barajas', 'Masculino');
+        console.log(x.getDescripcion());
+    }
+
+    testReservacion() {
+        let x1 = new Reservacion(12, new Date(2007, 2, 12), 3);
+        console.log(x1.getFechaFormatoCorte());
+        let h2 = new Huesped('Juan Pérez', 'Masculino');
+        let h1 = new Huesped('Jose Barajas', 'Masculino');
+        x1.addHuesped(h1);
+        x1.addHuesped(h2);
+        console.log(x1.getNumHuespedes());
+        console.log(x1.print());
+    }
+
+    testHotel() {
+        let x1 = new Reservacion(12, new Date(2007, 2, 12), 3);
+        x1.addHuesped(new Huesped('Juan Pérez', 'Masculino'));
+        x1.addHuesped(new Huesped('Jose Barajas', 'Masculino'));
+        let x2 = new Reservacion(12, new Date(2007, 2, 12), 3);
+        x2.addHuesped(new Huesped('Juan Juanjo', 'Masculino'));
+        x2.addHuesped(new Huesped('Jose Jose', 'Masculino'));
+    }
+}
+
+let app = new Main();
+app.testHuesped();
+app.testReservacion();
